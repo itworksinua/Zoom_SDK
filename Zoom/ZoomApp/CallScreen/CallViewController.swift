a9c1f61a3b79276513da2147651ff9bb761752b9
Dmitry Brick <>
2021-06-15 23:48:35 +0300

    sync with CallKit

---
+++ Zoom/ZoomApp/CallScreen/CallViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CallViewController.swift
+//  ZoomApp
+//
+//  Created by 2020 on 14.06.2021.
+//
+
+import UIKit
+
+class CallViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
